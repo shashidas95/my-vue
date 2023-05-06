c04f18440736d1f8ac7e0af7516c457e8305e9c5
shashidas95 <shashidas95@gmail.com>
2023-05-06 07:36:55 +0600

    setup vue

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Task tracker</h1>
+  </header>
+</template>
+<!-- scripts -->
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+<!-- styles -->
+<style scoped>
+header{
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ margin-bottom: 20px;
+}
+</style>
