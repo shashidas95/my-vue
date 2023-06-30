621013f39c72d5e03bda50e8a11d22fb443b354f
shashi kanta das <60028859+shashidas95@users.noreply.github.com>
2023-06-30 21:27:34 +0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
 })
